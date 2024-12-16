57b1944d989f2b355a78f68a7b98dde42bd7e9c0
Kirill Trezubov <kirill3zoff@gmail.com>
2024-12-16 17:24:20 +0500

    open-gl: Finish lesson 4

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -19,16 +21,31 @@ int main(void)
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
+  if (glewInit() != GLEW_OK) {
+	std::cout << "Error!" << std::endl;
+  }
+
+  std::cout << glGetString(GL_VERSION) << std::endl;
+
+  float positions[6] = {
+	-0.5f, -0.5f,
+	0.0f, 0.5f,
+	0.5f, -0.5f
+  };
+
+  unsigned int buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
   {
 	/* Render here */
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glBegin(GL_TRIANGLES);
-	glVertex2f(-0.5f, -0.5f);
-	glVertex2f(0.0f, 0.5f);
-	glVertex2f(0.5f, -0.5f);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
 	glEnd();
 
 	/* Swap front and back buffers */
